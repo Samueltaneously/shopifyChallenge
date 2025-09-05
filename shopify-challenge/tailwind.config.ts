9b4fe8e31b1053db41a2fd4cef32be65c8597005
Samuel Nelson <samuel.h.nelson@protonmail.com>
2025-09-05 11:58:32 -0500

    initial setup

---
+++ shopify-challenge/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+    content: [
+        './app/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+
+    plugins: [],
+} satisfies Config
